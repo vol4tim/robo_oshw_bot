64fcf693b062c25bf271597c6d090d38068257a1
vol4tim <vol4tim@gmail.com>
2023-11-30 16:53:56 +0400

    upd prices

--- src/products.js
+++ src/products.js
@@ -7,7 +7,7 @@ export const products = [
       // },
       "https://raw.githubusercontent.com/vol4tim/robo_oshw_bot/ce14ec071391b186b4e6e752e94732ccdca74484/images/P1012625.JPG",
     title: "Universal IR Remote Control",
-    price: 20,
+    price: 28,
     description:
       "Build smart climate control with any ANY A-N-Y air conditioner, setup open/close operations for any ANY A-N-Y gates in your home. We`ve built universal IR remote control with an open-source Tasmota firmware onboard. We installed USB-C port for your better experience - upgrade the firmware for the remote control just with a simple wired connection!",
     features: [
@@ -24,7 +24,7 @@ export const products = [
       // },
       "https://raw.githubusercontent.com/vol4tim/robo_oshw_bot/ce14ec071391b186b4e6e752e94732ccdca74484/images/P1012619.JPG",
     title: "Smart Energy Power Consumption Monitoring",
-    price: 38,
+    price: 40,
     description:
       "Have you ever wondered what consumes the most electricity inyour home? Our smart DIN energy power meter is designed to help you identify the major sources of electricity usage and prevent unwanted energy leaks. This energy meter works both, with an individual assert or an entirebuilding, doesn`t need any cloud, MQTT protocol onboard, can be used with Home Assistant and it`s useful energy management dashboard.",
     features: [
