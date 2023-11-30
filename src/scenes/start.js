7a868fa79fdb80d664792c1c13ac2eaf67a8f491
vol4tim <vol4tim@gmail.com>
2023-11-30 14:17:35 +0400

    add new user

--- src/scenes/start.js
+++ src/scenes/start.js
@@ -2,6 +2,18 @@ import bot from "../bot";
 import Profile from "../models/profile";
 
 export function start() {
+  bot.use(async (ctx, next) => {
+    const profile = await Profile.findOne({ where: { userId: ctx.from.id } });
+    if (profile === null) {
+      await Profile.create({
+        userId: ctx.from.id,
+        username: ctx.from.username,
+        firstName: ctx.from.first_name,
+        lastName: ctx.from.last_name
+      });
+    }
+    next();
+  });
   bot.start(async (ctx) => {
     const profile = await Profile.findOne({ where: { userId: ctx.from.id } });
     if (profile === null) {
