d792dd105c25ed04abad2eba00fa64f033901bbc
vol4tim <vol4tim@gmail.com>
2024-01-05 17:43:16 +0400

    forward messages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { SQLite } from "@telegraf/session/sqlite";
 import { Scenes, session } from "telegraf";
 import bot from "./bot";
-import { PATH_SESSION_DB } from "./config";
+import { PATH_SESSION_DB, config } from "./config";
 import { start as startCrypto } from "./merchant/crypto";
 import { app as startStripe } from "./merchant/stripe";
 import db from "./models/db";
@@ -50,6 +50,31 @@ const runApp = () => {
   myOrders();
   checkout();
   orders();
+
+  bot.on("message", async (ctx) => {
+    if (!config.adminsGroup) {
+      return;
+    }
+    if (
+      ctx.message.chat.type === "group" &&
+      ctx.message.chat.id === config.adminsGroup &&
+      ctx.message.reply_to_message &&
+      ctx.message.reply_to_message.from.id === config.bot.id
+    ) {
+      await ctx.telegram.sendMessage(
+        ctx.message.reply_to_message.forward_from.id,
+        ctx.message.text,
+        {
+          reply_to_message_id: ctx.message.reply_to_message.message_id - 1
+        }
+      );
+      return;
+    }
+    if (ctx.message.chat.type === "private") {
+      ctx.forwardMessage(config.adminsGroup);
+      return;
+    }
+  });
 
   bot.launch();
 };
